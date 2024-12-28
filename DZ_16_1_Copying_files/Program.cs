7ad2001ae2f2d164e9cf237b90e1951f101801e3
Vlad Ismailov <ismailovv527@gmail.com>
2024-12-28 12:54:38 +0200

    Hillel: Copyng files

---
+++ DZ_16_1_Copying_files/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace DZ_16_1_Copying_files
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+            try
+            {
+                Console.Write("Введіть шлях до файлу: ");
+                string sourcePath = Console.ReadLine();
+
+                // Перевіряємо, чи існує файл
+                FileInfo sourceFile = new FileInfo(sourcePath);
+                if (!sourceFile.Exists)
+                {
+                    Console.WriteLine("Файл не знайдено.");
+                    return;
+                }
+
+                // Запитуємо у користувача шлях до файлу призначення
+                Console.Write("Введіть шлях до папки, в який потрібно скопіювати файл: ");
+                string destinationPath = Console.ReadLine();
+
+                // Використовуємо StreamReader для читання та StreamWriter для запису
+                using (StreamReader reader = new StreamReader(sourcePath))
+                using (StreamWriter writer = new StreamWriter(destinationPath))
+                {
+                    string content = reader.ReadToEnd();
+                    writer.Write(content);
+                }
+
+                
+                Console.WriteLine("Файл успішно скопійовано!");
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine($"Помилка: {ex.Message}");
+            }
+        }
+    }
+}
