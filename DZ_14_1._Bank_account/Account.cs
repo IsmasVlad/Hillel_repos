290aa5c972e0077f5c92f8ac861cd6702136343c
Vlad Ismailov <ismailovv527@gmail.com>
2024-12-28 12:14:40 +0200

    Hillel: Bank account

---
+++ DZ_14_1._Bank_account/Account.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZ_14_1_Bank_account
+{
+    public class Account // Класс Account, представляющий банковский счет
+    {
+        public string Name { get; set; } // Название счета
+
+
+        private decimal balance; // Состояние счета
+
+        public Account(string name, decimal initialBalance) // Конструктор для инициализации счета
+        {
+            if (initialBalance < 0)
+                throw new ArgumentOutOfRangeException("Начальная сумма не может быть меньше 0.");
+
+            Name = name;
+            this.balance = balance;
+        }
+        public void AddMoney(decimal summ) // Метод для добавления денег на счет
+        {
+            if ( summ <= 0)
+                throw new ArgumentOutOfRangeException("Сумма добавления должна быть больше 0");
+
+                balance += summ;
+                Console.WriteLine($"На счет {Name} добавлено {summ:C}. Текущий баланс: {balance:C}.");
+        }
+
+        public void Withdraw(decimal summ) // Метод для снятия денег с счета
+        {
+            if (summ <= 0)
+                throw new ArgumentOutOfRangeException("Сумма снятия должна быть больше 0.");
+            balance -= summ;
+            Console.WriteLine($"Со счета {Name} снято {summ:C}. Текущий баланс: {balance:C}.");
+        }
+
+        public decimal GetBalance()
+        {
+            return balance;
+        }
+    }
+}
---
+++ DZ_14_1._Bank_account/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace DZ_14_1_Bank_account
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            try
+            {
+                Account myAccount = new Account("Основной счет", 1000);
+                Console.WriteLine($"Начальный баланс счета \"{myAccount.Name}\": {myAccount.GetBalance():C}\n");
+
+                myAccount.AddMoney(500);
+
+                myAccount.Withdraw(300);
+
+                Console.WriteLine($"Текущий баланс: {myAccount.GetBalance():C}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка: {ex.Message}");
+            }
+        }
+    }
+}
